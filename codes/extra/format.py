dc1d726b948089a2acc7105cd3616b526f775334
EmrysChe <704265711@qq.com>
2018-06-20 14:54:04 +0800

    add *

---
+++ codes/extra/format.py
@@ -0,0 +1,15 @@
+Str1 = "hello"
+Str2 = "world"
+Num = 32
+
+print('{0} {1}! Num is {2}.'.format(Str1,Str2,Num))
+
+print('{0:.2f}'.format(Num/3))
+
+print('{0:_^12}'.format('Python'))
+
+print('{one} {two}!'.format(one = 'hello',two = 'world'))
+
+print(Str1,end=' ')
+print(Str2,end='')
+print('!')
